20fa2ed05ec5c8f8648f17f914010ba026e683a2
Simon Collins <scollins@fastmail.fm>
2014-03-14 22:11:26 +1100

    Added listening for media query triggers

--- src/frame_content_script.js
+++ src/frame_content_script.js
@@ -4,15 +4,60 @@
  */
 if(window.name == 'seesponsive') {
 
-	console.debug("Frame content script loaded into iframe!");
+	function getLocalMediaQueries() {
+		var mediaQueries = [];
+		var sheets = document.styleSheets;
+		if(sheets != null) {
+			for(var i=0; i < sheets.length; i++) {
+				var rules = sheets[i].cssRules;
+				if(rules != null && sheets[i].href == null) {
+					console.debug("Found internal stylesheet. Looking for media queries");
+					for(var j=0; j < rules.length; j++) {
+						if(rules[j].constructor === CSSMediaRule) {
+							mediaQueries.push({
+								url: 'LOCAL',
+								text: rules[j].media.mediaText
+							});
+						}
+					}
+				}
+			}
+		}
+		console.debug("Returning " + mediaQueries.length + " internally declared media queries");
+		return mediaQueries;
+	}
+
+	function listenForMediaQueryChanges(mediaQueries) {
+		for(var i=0; i < mediaQueries.length; i++) {
+			
+			var mql = window.matchMedia(mediaQueries[i].text);
+      mql.addListener(function(q) {
+      	console.debug("Media query triggered: " + q.media);
+      	if(q.matches) {
+      		console.debug("... condition was met");
+      	} else {
+      		console.debug("... condition was not met");
+      	}
+      });
+		}
+	}
 
 	// Listen for the event page giving us our media queries
 	chrome.runtime.onMessage.addListener(function listener(request) {
 		if(request.action == 'mediaQueriesLoaded') {
-			var mediaQueries = request.mediaQueries;
+			var mediaQueries = request.mediaQueries;			
+			chrome.runtime.onMessage.removeListener(listener);
+
+			// we have media queries from external stylesheets. Now merge in any from styletags
+			// in the page
+			mediaQueries = mediaQueries.concat(getLocalMediaQueries());			
 			console.debug(mediaQueries.length + " media queries were found for the site");
 			console.dir(mediaQueries);
-			chrome.runtime.onMessage.removeListener(listener);
+
+			// setup listeners for each of the queries being met
+			listenForMediaQueryChanges(mediaQueries);
 		}
 	});
+
+	console.debug("Frame content script loaded into iframe!");
 }
